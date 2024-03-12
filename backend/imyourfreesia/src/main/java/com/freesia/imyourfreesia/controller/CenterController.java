953eaf0f192c394f3c805a9c238b119498dbd802
GaGa-Kim <kgg3156@gmail.com>
2024-03-12 17:58:34 +0900

    [back] Refactor CenterService

--- backend/imyourfreesia/src/main/java/com/freesia/imyourfreesia/controller/CenterController.java
+++ backend/imyourfreesia/src/main/java/com/freesia/imyourfreesia/controller/CenterController.java
@@ -6,32 +6,35 @@ import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiImplicitParam;
 import io.swagger.annotations.ApiOperation;
 import java.util.List;
+import javax.validation.constraints.NotNull;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
-@Api(tags = {"Center API"})
-@RequiredArgsConstructor
+@Api(tags = {"Center API (샌터 API)"})
 @RestController
+@Validated
+@RequiredArgsConstructor
 @CrossOrigin(origins = "http://localhost:3000")
 public class CenterController {
-    private final CenterService centerServiceImpl;
+    private final CenterService centerService;
 
     @GetMapping("/center")
     @ApiOperation(value = "지역 센터 정보 검색", notes = "지역 센터 정보 검색 API")
     @ApiImplicitParam(name = "address", value = "주소")
-    public ResponseEntity<List<Center>> searchCenter(@RequestParam String address) {
-        return ResponseEntity.ok().body(centerServiceImpl.findCenterListByAddress(address));
+    public ResponseEntity<List<Center>> searchCenter(@RequestParam @NotNull String address) {
+        return ResponseEntity.ok().body(centerService.findCenterListByAddress(address));
 
     }
 
     @GetMapping("/centers")
     @ApiOperation(value = "지역 센터 정보 전체 조회", notes = "지역 센터 전체 조회 API")
     public ResponseEntity<List<Center>> totalCenter() {
-        return ResponseEntity.ok().body(centerServiceImpl.findAllCenterList());
+        return ResponseEntity.ok().body(centerService.findAllCenterList());
 
     }
 }
