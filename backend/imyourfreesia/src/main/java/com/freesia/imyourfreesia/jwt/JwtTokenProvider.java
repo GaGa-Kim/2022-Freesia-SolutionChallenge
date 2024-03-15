168174953a8895af4115235a6dda5f82e4d428b4
GaGa-Kim <kgg3156@gmail.com>
2024-03-16 03:03:14 +0900

    [back] Security 허용 변경

--- backend/imyourfreesia/src/main/java/com/freesia/imyourfreesia/jwt/JwtTokenProvider.java
+++ backend/imyourfreesia/src/main/java/com/freesia/imyourfreesia/jwt/JwtTokenProvider.java
@@ -1,10 +1,11 @@
 package com.freesia.imyourfreesia.jwt;
 
 import com.freesia.imyourfreesia.config.GlobalConfig;
+import com.freesia.imyourfreesia.domain.user.User;
+import io.jsonwebtoken.Claims;
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.SignatureAlgorithm;
 import java.util.Date;
-import java.util.HashMap;
 import javax.servlet.http.HttpServletResponse;
 import org.springframework.stereotype.Component;
 
@@ -20,11 +21,12 @@ public class JwtTokenProvider {
         this.expirationTime = config.getExpirationTime() * 1000;
     }
 
-    public String generateToken(String email) {
+    public String generateToken(User user) {
+        Claims claims = Jwts.claims().setSubject(user.getEmail());
+        claims.put("roles", user.getRole());
         Date now = new Date();
         return Jwts.builder()
-                .setClaims(new HashMap<>())
-                .setSubject(email)
+                .setClaims(claims)
                 .setIssuedAt(now)
                 .setExpiration(new Date(now.getTime() + expirationTime))
                 .signWith(SignatureAlgorithm.HS512, secret)
